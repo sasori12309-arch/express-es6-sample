fd56070d4732b33940e3d33667a506552cbced00
sasori12309-arch <sasori12309@gmail.com>
2025-11-02 15:44:19 +0800

    Fix export statement in index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.send('My Student ID is 21938690. Welcome to Express');
 });
